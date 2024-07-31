007fef91a98f198164c777211db1b147bd6a9783
Neil Smith <neil@nsmith.co.uk>
2024-07-31 07:00:35 +0100

    Added tree format for CLI

---
+++ go.mod
@@ -0,0 +1,23 @@
+module dns-lookup-go-trace
+
+go 1.22
+
+require (
+	github.com/jedib0t/go-pretty/v6 v6.5.9
+	github.com/nsmithuk/dns-lookup-go v1.1.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/miekg/dns v1.1.61 // indirect
+	github.com/nsmithuk/dns-anchors-go v1.1.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	golang.org/x/mod v0.19.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/tools v0.23.0 // indirect
+)
---
+++ trace/output.go
@@ -0,0 +1,70 @@
+package trace
+
+import (
+	"fmt"
+	"github.com/jedib0t/go-pretty/v6/list"
+	"github.com/jedib0t/go-pretty/v6/text"
+	"github.com/nsmithuk/dns-lookup-go/lookup"
+)
+
+func PrintTrace(trace *lookup.Trace) {
+
+	text.EnableColors()
+
+	headerText := text.Colors{text.Bold, text.Italic, text.FgHiMagenta}
+	greenText := text.Colors{text.FgGreen}
+	faintText := text.Colors{text.Faint}
+
+	//---
+
+	l := list.NewWriter()
+	l.SetStyle(list.StyleConnectedRounded)
+
+	for _, entry := range trace.Records {
+		switch r := entry.(type) {
+		case lookup.TraceLookup:
+			l.AppendItem(headerText.Sprint("DNS Lookup"))
+			l.Indent()
+			l.AppendItem(fmt.Sprintf("%s: %s %s", greenText.Sprint("for"), r.Rrtype, r.Domain))
+			l.AppendItem(fmt.Sprintf("%s: %s", greenText.Sprint("on"), r.Nameserver))
+			l.AppendItem(fmt.Sprintf("%s: %s", greenText.Sprint("taking"), r.Latency))
+			l.AppendItem(fmt.Sprintf("%s:", greenText.Sprint("answers")))
+			l.Indent()
+			for _, r := range r.Answers {
+				l.AppendItem(faintText.Sprint(r))
+			}
+			l.UnIndent()
+			l.UnIndent()
+		case lookup.TraceSignatureValidation:
+			l.Indent()
+			l.AppendItem(headerText.Sprint("Signature Validation"))
+			l.Indent()
+			l.AppendItem(fmt.Sprintf("%s: %s", greenText.Sprint("type"), r.KeyType))
+			l.AppendItem(fmt.Sprintf("%s: %s", greenText.Sprint("for"), r.Domain))
+			l.AppendItem(fmt.Sprintf("%s: %s", greenText.Sprint("in"), r.Zone))
+			l.AppendItem(fmt.Sprintf("%s: %s", greenText.Sprint("key"), r.Key))
+			l.AppendItem(fmt.Sprintf("%s: %s", greenText.Sprint("signature"), r.Signature))
+			l.AppendItem(fmt.Sprintf("%s: %s", greenText.Sprint("hash"), r.KeySha256))
+			l.AppendItem(fmt.Sprintf("%s:", greenText.Sprint("records")))
+			l.Indent()
+			for _, r := range r.Records {
+				l.AppendItem(faintText.Sprint(r))
+			}
+			l.UnIndent()
+			l.UnIndent()
+			if r.KeyType == "zsk" {
+				l.UnIndent()
+			}
+		case lookup.TraceDelegationSignerCheck:
+			l.Indent()
+			l.AppendItem(headerText.Sprint("Delegation Signer Check"))
+			l.Indent()
+			l.AppendItem(fmt.Sprintf("%s: %s", greenText.Sprint("child"), r.Child))
+			l.AppendItem(fmt.Sprintf("%s: %s", greenText.Sprint("parent"), r.Parent))
+			l.AppendItem(fmt.Sprintf("%s: %s", greenText.Sprint("hash"), r.Hash))
+			l.UnIndent()
+		}
+	}
+
+	fmt.Println(l.Render())
+}
