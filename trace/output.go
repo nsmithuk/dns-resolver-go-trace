ce6b6cf89fabb3f8c09a4996515bffa992233532
Neil Smith <neil@nsmith.co.uk>
2024-08-03 20:14:31 +0100

    Added support for RecursiveQueryTraceLookup

--- trace/output.go
+++ trace/output.go
@@ -4,10 +4,74 @@ import (
 	"fmt"
 	"github.com/jedib0t/go-pretty/v6/list"
 	"github.com/jedib0t/go-pretty/v6/text"
-	"github.com/nsmithuk/dns-lookup-go/lookup"
+	"github.com/nsmithuk/dns-lookup-go/resolver"
 )
 
-func GetConsoleTree(trace *lookup.Trace) string {
+func GetRecursiveQueryConsoleTree(trace *resolver.RecursiveQueryTrace) string {
+	text.EnableColors()
+
+	headerText := text.Colors{text.Bold, text.Italic, text.FgHiMagenta}
+	greenText := text.Colors{text.FgGreen}
+	faintText := text.Colors{text.Faint}
+
+	//---
+
+	l := list.NewWriter()
+	l.SetStyle(list.StyleConnectedRounded)
+
+	var c uint8 = 0
+	for _, entry := range trace.Records {
+		switch r := entry.(type) {
+		case resolver.RecursiveQueryTraceLookup:
+			if r.Depth < c {
+				for range c - r.Depth {
+					l.UnIndent()
+				}
+				c = r.Depth
+			}
+
+			l.AppendItem(headerText.Sprint("DNS Query"))
+			l.Indent()
+			//l.AppendItem(fmt.Sprintf("%s: %d", greenText.Sprint("depth"), r.Depth))
+			l.AppendItem(fmt.Sprintf("%s: %s %s", greenText.Sprint("for"), r.Domain, r.Rrtype))
+			l.AppendItem(fmt.Sprintf("%s: %s %s", greenText.Sprint("on"), r.ServerHost, r.ServerUri))
+			l.AppendItem(fmt.Sprintf("%s: %s", greenText.Sprint("took"), r.Latency))
+
+			if len(r.Answers) > 0 {
+				l.AppendItem(fmt.Sprintf("%s:", greenText.Sprint("answers found")))
+				l.Indent()
+				for _, r := range r.Answers {
+					l.AppendItem(faintText.Sprint(r))
+				}
+				l.UnIndent()
+			}
+
+			if len(r.Nameservers) > 0 {
+				l.AppendItem(fmt.Sprintf("%s:", greenText.Sprint("nameservers found")))
+				l.Indent()
+				for _, r := range r.Nameservers {
+					l.AppendItem(faintText.Sprint(r))
+				}
+				l.UnIndent()
+			}
+
+			if len(r.Extra) > 0 {
+				l.AppendItem(fmt.Sprintf("%s:", greenText.Sprint("extra records returned")))
+				l.Indent()
+				for _, r := range r.Extra {
+					l.AppendItem(faintText.Sprint(r))
+				}
+				l.UnIndent()
+			}
+
+		}
+		c += 1
+	}
+
+	return l.Render()
+}
+
+func GetAuthenticationConsoleTree(trace *resolver.AuthenticationTrace) string {
 	text.EnableColors()
 
 	headerText := text.Colors{text.Bold, text.Italic, text.FgHiMagenta}
@@ -21,8 +85,8 @@ func GetConsoleTree(trace *lookup.Trace) string {
 
 	for _, entry := range trace.Records {
 		switch r := entry.(type) {
-		case lookup.TraceLookup:
-			l.AppendItem(headerText.Sprint("DNS Lookup"))
+		case resolver.AuthenticationTraceLookup:
+			l.AppendItem(headerText.Sprint("DNS resolver"))
 			l.Indent()
 			l.AppendItem(fmt.Sprintf("%s: %s %s", greenText.Sprint("for"), r.Rrtype, r.Domain))
 			l.AppendItem(fmt.Sprintf("%s: %s", greenText.Sprint("on"), r.Nameserver))
@@ -34,7 +98,7 @@ func GetConsoleTree(trace *lookup.Trace) string {
 			}
 			l.UnIndent()
 			l.UnIndent()
-		case lookup.TraceSignatureValidation:
+		case resolver.AuthenticationTraceSignatureValidation:
 			l.Indent()
 			l.AppendItem(headerText.Sprint("Signature Validation"))
 			l.Indent()
@@ -55,7 +119,7 @@ func GetConsoleTree(trace *lookup.Trace) string {
 			if r.KeyType == "zsk" {
 				l.UnIndent()
 			}
-		case lookup.TraceDelegationSignerCheck:
+		case resolver.AuthenticationTraceDelegationSignerCheck:
 			l.Indent()
 			l.AppendItem(headerText.Sprint("Delegation Signer Check"))
 			l.Indent()
